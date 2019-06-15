f39abd3f6f9c0131bb0a8973cbe537f888cdc2d3
Pierre-Luc Gagné <pierreluc212@gmail.com>
2019-06-15 13:50:06 +0900

    profanity filter

--- src/app/components/Notification/index.tsx
+++ src/app/components/Notification/index.tsx
@@ -3,6 +3,7 @@
 import * as React from 'react';
 import { INotification } from '../../redux/modules/notifications';
 import { ISettings } from '../../redux/modules/settings';
+import { CensorSensor } from 'censor-sensor';
 
 const style = require('./style.scss');
 
@@ -13,13 +14,29 @@ interface IProps {
 }
 
 class NotificationC extends React.Component<IProps> {
+  private censor: CensorSensor;
 
   constructor(props: IProps) {
     super(props);
+
+    this.censor = new CensorSensor();
+    this.censor.enableTier(1);
   }
 
   public render() {
     const { settings, notification, display } = this.props;
+
+    let username = '';
+    let message = '';
+    if (notification) {
+      username = notification.username;
+      message = notification.message;
+
+      if (settings.profanityFilter) {
+        username = this.censor.cleanProfanity(username);
+        message = this.censor.cleanProfanity(message);
+      }
+    }
 
     return (
       <div className={style.notification}>
@@ -27,7 +44,7 @@ class NotificationC extends React.Component<IProps> {
           {notification && settings.showImage && <img src={`${(settings.notificationImageUrl) ? settings.notificationImageUrl : 'assets/images/default.png'}`} />}
           {notification &&
             <div>
-              Thank you <span className={style.sourceName}>{notification.username}</span> for requesting <span className={style.sourceName}>{notification.request}</span> for <span className={style.sourcePrice}>{notification.amount}$</span> {(notification.message) ? `Message: ${notification.message}` : ''}
+              Thank you <span className={style.sourceName}>{username}</span> for requesting <span className={style.sourceName}>{notification.request}</span> for <span className={style.sourcePrice}>{notification.amount}$</span> {(message) ? `Message: ${message}` : ''}
             </div>}
         </div>
       </div>
